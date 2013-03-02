bef5ef3b027b7237a028ef6dfedc085d0675be7b
Thomas Mayer <thomas@residuum.org>
2013-03-02 19:52:21 +0100

    Setting tray icon.

--- tablet_trayicon.c
+++ tablet_trayicon.c
@@ -9,7 +9,7 @@ GtkStatusIcon *create_tray_icon() {
 
 	tray_icon = gtk_status_icon_new();
 	g_signal_connect(G_OBJECT(tray_icon), "activate", G_CALLBACK(tray_icon_on_click), NULL);
-	gtk_status_icon_set_from_icon_name(tray_icon, GTK_STOCK_MEDIA_STOP);
+	gtk_status_icon_set_from_icon_name(tray_icon, "input-tablet");
 	gtk_status_icon_set_tooltip(tray_icon, "Tablet mode switcher");
 	gtk_status_icon_set_visible(tray_icon, TRUE);
 
